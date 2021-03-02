78f77c3f9dd0ec13822fe817a76fb188c4da5474
Ramon Fernandes <ramonmfb777@gmail.com>
2021-03-02 12:00:20 -0300

    exercicio conversor de moedas

---
+++ ExerciciosMembrosEstaticos/ConversorDeMoedas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ExerciciosMembrosEstaticos
+{
+    class ConversorDeMoedas
+    {
+        public static double Iof = 0.06;
+
+        public static double DolarReal(double qtd, double cotacao)
+        {
+            double total = qtd * cotacao;
+            return total + total * Iof;
+        }
+    }
+}
---
+++ ExerciciosMembrosEstaticos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosMembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a cotação do dólar?");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantos dólares você vai comprar?");
+            double qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double r = ConversorDeMoedas.DolarReal(qtd, cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais: " + r.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
